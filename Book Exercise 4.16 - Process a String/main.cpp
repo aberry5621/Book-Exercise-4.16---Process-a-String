e58aca7f78c19a48767c0aa82c042ddb985e48ad
alexberry721 <alexberry721@gmail.com>
2016-09-12 12:55:08 -0700

    Init commit

--- Book Exercise 4.16 - Process a String/main.cpp
+++ Book Exercise 4.16 - Process a String/main.cpp
@@ -6,10 +6,25 @@
 //  Copyright © 2016 COMP130. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Process a String, Find Length and First Character \n";
+    
+    // Data and Initialization
+    
+    string input_string = " ";
+    
+    // I
+    cout << "Enter a string: ";
+    cin >> input_string;
+    
+    // P 0
+    cout << "String length: " << input_string.length() << endl;
+    cout << "The first character is: " << input_string.at(0) << endl;
+    
     return 0;
 }
